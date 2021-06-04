57bffb0db850339dea82eeb0187841d9b80b5be1
Unicodist <47056442+Unicodist@users.noreply.github.com>
2021-06-04 15:59:33 +0545

    V2
    Added gif command

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import os
 import discord
 import requests
+import giphy_client
+import random
 
 
 anime_commands = ['waifu','hug','kill','punch','kiss','slap','wink','pat','cuddle','boobs','lesbian','hentai']
 anime_commands_nsfw = anime_commands[-3:]
 print(anime_commands_nsfw)
 client = discord.Client()
+gif_client = giphy_client.DefaultApi()
 nsfw = False
 
 
@@ -58,6 +60,15 @@ async def on_message(message):
         nsfw = not nsfw
         await message.channel.send(
             f"NSFW mode has been {'enabled' if nsfw else 'disabled'}.")
+
+    if message.content.startswith('$gif'):
+        gifs = gif_client.gifs_search_get("dvIEwJsXejfeH8DP9Anqmw6X9CoLK0wZ",message.content[5:],rating='r')
+        gif_list = list(gifs.data)
+        await message.channel.send(random.choice(gif_list).embed_url)
+    
+    if message.content.startswith('$insult'):
+        msg = message.content[8:]
+        
 
 
 client.run("ODQ2OTY0MzAzMTIyOTIzNTgx.YK3K-Q.0ZJmrmFl0lRfPrOR3DKalNuNWfA")
