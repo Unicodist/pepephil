889da5ecfcf1287c598a1307f385ef62e8e8ebf8
Unicodist <47056442+Unicodist@users.noreply.github.com>
2021-06-04 08:07:34 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,104 +1,63 @@
-from datetime import date
-import sys
-import random
-import discord
-import sqlite3
-from discord_slash import SlashCommand, SlashCommandOptionType, SlashContext
-from discord.ext import commands
 import os
+import discord
+import requests
 
+
+anime_commands = ['waifu','hug','kill','punch','kiss','slap','wink','pat','cuddle','boobs','lesbian','hentai']
+anime_commands_nsfw = anime_commands[-3:]
+print(anime_commands_nsfw)
 client = discord.Client()
-slash = SlashCommand(client,sync_commands=True)
-comms = commands.Bot(command_prefix="$")
-conn = sqlite3.connect('quotes.db')
-cursor = conn.cursor()
+nsfw = False
 
 
-async def displayquotes(message):
-    print('display quote requested')
-    cursor.execute("SELECT rowid, quote FROM quotes")
-    text = ""
-    for item in cursor.fetchall():
-        text = text + str(item[0])+":"+item[1]+"\n"
-    return text
-
-
-def getrandomquotes():
-    cursor.execute("SELECT rowid,quote,date FROM quotes")
-    print(cursor.fetchall())
-    cursor.execute("SELECT rowid,quote,date FROM quotes")
-    choice = random.choice(cursor.fetchall())
-    print(choice)
-    conn.commit()
-    return choice[1]
-
-
-def addquotes(text):
-    today = date.today()
-    today = today.strftime('%Y/%b/%d')
-    cursor.execute("INSERT INTO quotes VALUES('1','{0}','{1}')".format(text, today))
-    conn.commit()
-
-
-def deleteRow(rowid):
-    cursor.execute("DELETE FROM quotes WHERE rowid = {0}".format(rowid))
-    conn.commit()
-
-
-# slash commands overrides:
-
-
-@slash.slash(description="Sends a random funny quote")
-async def pepesays(ctx):
-    await ctx.send("This works bro!")
-
-
-@slash.slash(description="Lists all the available quotes with row id")
-async def pepelist(ctx):
-    await ctx.send(displayquotes())
-    
-
-
-@slash.slash(description="shows available commands and a brief description of the bot")
-async def pepehelp(ctx):
-    print('help command recieved')
-    response = """
-        Hello, Pepe bot was originally created as a learning process of discord bots.
-        It is designed by Ashish Neupane (Unicodist)
-        
-        Available commands:
-        */help* Displays this text
-        */pepesays* Says something very wise
-        *$pepe add ...* Adds a new wiseness into the database
-        */pepelist* Lists the available quotes with their corresponding row number
-        *$pepe delete <int>* Deletes a quotes corresponding to the given row number
-    """
-    await ctx.send(response)
-
-# Slash commands overrides end
-# Client events start
+async def anime(message):
+  global nsfw
+  global anime_commands
+  msg = message.content[7:]
+  if msg in anime_commands:
+    if msg in anime_commands_nsfw:
+      if nsfw:
+        print("nsfw part")
+        response = requests.get(f'https://anime-api.hisoka17.repl.co/img/nsfw/{msg}').json()
+      else:
+        await message.channel.send("NSFW is disabled bro :D")
+        return
+    else:
+      response = requests.get(f'https://anime-api.hisoka17.repl.co/img/{msg}').json()
+    picture = f"{response['url']}{'.jpg' if msg=='waifu' else ''}"
+    await message.channel.send(picture)
+  else:
+    await message.channel.send(f""" Wrong command!
+            Use one of these:
+            {anime_commands}
+    """)
 
 
 @client.event
 async def on_ready():
-    print("bot is online")
-
+    print("READY")
 
 @client.event
 async def on_message(message):
+    global nsfw
+
     if message.author == client.user:
         return
-    if message.content.startswith('$pepe'):
-        msg = message.content[6:]
-        if msg.startswith("add"):
-            msg = " ".join(msg.split(" ")[1:])
-            addquotes(msg)
-            await message.channel.send("The quote ***{0}*** has been added to out database".format(msg))
-            return
-        if msg.startswith("delete"):
-            deleteRow(msg[-1])
-            return
+
+    if message.content.startswith('$anime '):
+        await anime(message)
+
+    if message.content.startswith('$joke'):
+        response = requests.get(
+            'https://official-joke-api.appspot.com/jokes/random').json()
+        await message.channel.send(f"""{message.author.mention}
+    **{response['setup']}**
+    *{response['punchline']}*""")
+
+    if message.content.startswith('$nsfw'):
+        nsfw = not nsfw
+        await message.channel.send(
+            f"NSFW mode has been {'enabled' if nsfw else 'disabled'}.")
 
 
-# Client events end
 client.run("ODQ2OTY0MzAzMTIyOTIzNTgx.YK3K-Q.0ZJmrmFl0lRfPrOR3DKalNuNWfA")
